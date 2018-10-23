5ef351e7d80a2aace4adf892cb3f952bc3300982
Carrie Scott <carriescott@Carries-MacBook-Pro.local>
2018-10-23 13:49:29 -0500

    Created elements using dynamic data

--- 01-Templates/zinc.js
+++ 01-Templates/zinc.js
@@ -4,7 +4,48 @@
 
 (() => {
     function populateList(results) {
-        console.log(results); // eslint-disable-line no-console
+        console.log(results);
+
+        function capFirst(words){
+            let splitWords = words.split(' ').map( word => {
+                return word[0].toUpperCase()+word.slice(1);
+            })
+            return splitWords.join(' ');
+        }
+
+        let container = document.getElementsByClassName('container')[0];
+        let userList = document.createElement('ul');
+        container.appendChild(userList);
+        userList.setAttribute('class','user-list');
+        userList.setAttribute('id','z-user-list');
+        for (let result of results) {
+            let user = document.createElement('li');
+            user.setAttribute('class','user');
+            userList.appendChild(user);
+
+                let userPhoto = document.createElement('img');
+                user.appendChild(userPhoto)
+                userPhoto.setAttribute('class','user-photo');
+                userPhoto.setAttribute('src', `${result.picture.large}`);
+                userPhoto.setAttribute('alt',`Photo of ${result.name.first} ${result.name.last}`);
+
+                let userName = document.createElement('div');
+                user.appendChild(userName);
+                userName.setAttribute('class','user-name');
+                userName.textContent = `${capFirst(result.name.first)} ${capFirst(result.name.last)}`;
+
+                let userLocation = document.createElement('div');
+                user.appendChild(userLocation);
+                userLocation.setAttribute('class','user-location');
+                userLocation.textContent = `${capFirst(result.location.city)}, ${capFirst(result.location.state)}`;
+
+                let userEmail = document.createElement('div');
+                user.appendChild(userEmail);
+                userEmail.setAttribute('class','user-email');
+                userEmail.textContent = `${result.email}`;
+
+        }
+        
     }
 
     function init() {
