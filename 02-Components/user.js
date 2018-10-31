5d0f801e1d090f5077135ac60313b392449e7472
Carrie Scott <carriescott@Carries-MacBook-Pro.local>
2018-10-31 13:58:25 -0500

    recursion is cool

--- 02-Components/user.js
+++ 02-Components/user.js
@@ -4,32 +4,31 @@
 /* globals Zinc */
 
 (() => {
-    function onClick() {
+    function userController() {
         this.addEventListener('click', () => {
             this.classList.toggle('hilight');
-            this.$state.hilight = !this.$state.hilitght;
+            this.$state.hilit = !this.$state.hilit;
         });
     }
 
     function populateList(users) {
         const myComponents = ['user-one', 'user-two', 'user-three', 'user-four', 'user-five'];
         for (let i = 0; i < users.length; i++) {
-            
-            //Zinc.register will now take the config object
             Zinc.registerComponent({
-                componentName: myComponents[i],
+                name: myComponents[i],
                 templateFile: 'user-item',
                 data: users[i],
-                controller: onClick
-            })
+                controller: userController
+            });
         }
         Zinc.registerComponent({
-            componentName: 'user-list',
-            templateFile: 'user-list'
-        })
+            name: 'user-list',
+            templateFile: 'user-list',
+        });
+        Zinc.renderComponents();
     }
 
-    fetch('https://randomuser.me/api/?results=1')
+    fetch('https://randomuser.me/api/?results=5')
         .then(res => res.json())
         .then(json => populateList(json.results));
 })();
--- 02-Components/zinc.js
+++ 02-Components/zinc.js
@@ -1,61 +1,64 @@
 'use strict';
 
+/* eslint-env browser */
+/* eslint-disable no-unused-vars */
+
 const Zinc = {
-    _components: {}
+    components: {}
 };
 
 (() => {
     const domParser = new DOMParser();
-    Zinc._components = {};
-    document.addEventListener('DOMContentLoaded', init)
-    
-    // i want to pass register component the details and have it register, saving all the data.
-    // render Template will be performed given the registered component key-values 
-    //then i want to go through each of the components and render each one given those values. 
-    
-    function renderTemplate (templateFile, data) {
-        console.log('renderTemplate was called')
-        return fetch (templateFile+'.html')
-        .then ( res => res.text())
-        .then (html => html.replace(/\{\{\s*(.*?)\s*\}\}/g, (match, variable) => 
-                variable.split('.').reduce((acc, curr) => acc[curr],data)))
-        .then (rendered => console.log('rendered html: ', rendered))
-    }
-
-    function renderComponent (componentName) {
-        console.log('renderComponent was called');
-        console.log(componentName)
-        const component = Zinc._components[componentName];
-        const nodeList = document.querySelectorAll(componentName);
-        nodeList.forEach( (node) => {
-            console.log('nodeList.forEach is beginning- next should be render template')
-            renderTemplate(component.templateFile, component.data)
-            .then ((html) => {
-                const doc = domParser.parseFromString(html, 'text/html');
-                console.log('doc: ',doc.firstChild.children[1])
-                const elem = node.insertAdjacentElement('beforeend', doc.firstChild.children[1]);
-                // ^ changed second param of elem. used to be doc.firstChild.children[1].firstChild
-                elem._state = {};
-                if (component.controller) {
-                    elem.controller = component.controller;
-                    elem.controller();
-                };
-                Zinc._components[componentName].element = elem;
-            });
-        });
-    }
-    
-    //this function will now take one config object instead of 4 arguments
-    Zinc.registerComponent = function (configObj) {
-        Zinc._components[configObj.componentName] = {
-            componentName: configObj.componentName,
-            templateFile: configObj.templateFile,
-            data: configObj.data,
-            controller: configObj.controller
-        };
-        renderComponent (configObj.componentName);
-    }
 
     function init() {
-    };   
+        Zinc.renderComponents();
+    }
+
+    // TBD: prefetch and cache templates?
+    function renderTemplate(template, data) {
+        return fetch(`${template}.html`)
+            .then(res => res.text())
+            .then(html => html.replace(/\{\{\s*([\w.]+)\s*\}\}/g, (match, variable) =>
+                variable.split('.').reduce((acc, curr) => acc[curr], data) || ''));
+    }
+
+    Zinc.renderComponent = (componentName, parentNode = document) => {
+        const component = Zinc.components[componentName];
+        const nodeList = parentNode.querySelectorAll(componentName);
+        for (let i = 0; i < nodeList.length; i++) {
+            renderTemplate(component.templateFile, component.data)
+                .then((html) => {
+                    const doc = domParser.parseFromString(html, 'text/html');
+                    const el = nodeList[i].insertAdjacentElement('beforeend', doc.firstChild.children[1].firstChild);
+                    el.$state = {};
+                    if (component.controller) {
+                        el.$controller = component.controller;
+                        el.$controller();
+                    }
+                    Zinc.renderComponents(el);
+                });
+        }
+    };
+
+    Zinc.renderComponents = (rootNode = document) => {
+        Object.values(Zinc.components).forEach((component) => {
+            Zinc.renderComponent(component.name, rootNode);
+        });
+    };
+
+    Zinc.registerComponent = ({
+        name,
+        templateFile,
+        data,
+        controller
+    }) => {
+        Zinc.components[name] = {
+            name,
+            templateFile,
+            data,
+            controller
+        };
+    };
+
+    document.addEventListener('DOMContentLoaded', init);
 })();
